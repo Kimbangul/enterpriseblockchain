b5c5ae973de7ce34243495d852da3774d71a6fa4
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-05-27 10:42:25 +0900

    feat: scrolltrigger 애니메이션 추가

--- js/script.js
+++ js/script.js
@@ -1,38 +1,59 @@
-const setHeaderPosition = () => {
-  const header = document.querySelector('.header');
+const getPageViewport = () => {
+  return window.innerHeight;
+};
 
-  // if (window.scrollY >= window.innerHeight) {
-  if (window.scrollY >= 50) {
-    header.dataset.show = 'true';
-  } else {
-    header.dataset.show = 'false';
-  }
+const setHeaderPosition = () => {
+  ScrollTrigger.create({
+    trigger: 'body',
+    start: window.innerHeight / 2,
+    toggleClass: { targets: '.header', className: 'header--show' },
+  });
 };
 
 const introAnimation = () => {
   const introDesc = gsap.utils.toArray('.intro__desc');
+
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: '.intro',
-      markers: true,
       pin: true,
-      end: `+=${introDesc.length * 100}vh`,
+      yoyo: true,
+      scrub: true,
+      end: `+=${(introDesc.length + 1) * window.innerHeight}`,
+    },
+  });
+  const tl2 = gsap.timeline({
+    scrollTrigger: {
+      targets: '.sc__inner',
+      trigger: '.intro',
+      yoyo: true,
+      scrub: true,
+      markers: true,
+      start: 0,
+      end: window.innerHeight / 2,
+      //end: `${window.innerHeight}`,
     },
   });
 
-  tl.addLabel('start').from(introDesc, {
-    opacity: 0,
+  tl2.to('.sc__inner', {
+    background: 'rgba(0,0,0,0.6)',
+  });
+
+  const setSection = introDesc.forEach((el, idx) => {
+    tl.to(el, { opacity: 1 });
+    tl.to(el, { opacity: 0 });
   });
 };
 
 // FUNCTION gsap 애니메이션 등록
 const registAnimation = () => {
   introAnimation();
+  setHeaderPosition();
 };
 
 (function () {
   // 로드 후 즉시 실행
   console.log('load');
-  window.addEventListener('scroll', setHeaderPosition);
+  // window.addEventListener('scroll', setHeaderPosition);
   registAnimation();
 })();
