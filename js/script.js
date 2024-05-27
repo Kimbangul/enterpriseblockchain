91f31571f39d0bfdaffa2fb9f74e0f0012feaefe
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-05-27 12:01:17 +0900

    style: add footer

--- js/script.js
+++ js/script.js
@@ -2,53 +2,58 @@ const getPageViewport = () => {
   return window.innerHeight;
 };
 
-const setHeaderPosition = () => {
-  ScrollTrigger.create({
-    trigger: 'body',
-    start: window.innerHeight / 2,
-    toggleClass: { targets: '.header', className: 'header--show' },
-  });
-};
+const animation = {
+  setHeaderPosition: () => {
+    ScrollTrigger.create({
+      trigger: 'body',
+      start: window.innerHeight / 2,
+      toggleClass: { targets: '.header', className: 'header--show' },
+    });
+  },
+  setIntro: () => {
+    const introDesc = gsap.utils.toArray('.intro__desc');
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.intro',
+        pin: true,
+        yoyo: true,
+        scrub: true,
+        end: `+=${(introDesc.length + 1) * window.innerHeight}`,
+      },
+    });
+    const tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.intro',
+        yoyo: true,
+        scrub: true,
+        start: 0,
+        end: window.innerHeight / 2,
+        //end: `${window.innerHeight}`,
+      },
+    });
 
-const introAnimation = () => {
-  const introDesc = gsap.utils.toArray('.intro__desc');
-
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.intro',
-      pin: true,
-      yoyo: true,
-      scrub: true,
-      end: `+=${(introDesc.length + 1) * window.innerHeight}`,
-    },
-  });
-  const tl2 = gsap.timeline({
-    scrollTrigger: {
-      targets: '.sc__inner',
-      trigger: '.intro',
-      yoyo: true,
-      scrub: true,
-      markers: true,
-      start: 0,
-      end: window.innerHeight / 2,
-      //end: `${window.innerHeight}`,
-    },
-  });
-
-  tl2.to('.sc__inner', {
-    background: 'rgba(0,0,0,0.6)',
-  });
-
-  const setSection = introDesc.forEach((el, idx) => {
-    tl.to(el, { opacity: 1 });
-    tl.to(el, { opacity: 0 });
-  });
+    tl2.to('.intro .sc__inner', {
+      background: 'rgba(0,0,0,0.6)',
+    });
+    introDesc.forEach((el, idx) => {
+      tl.to(el, { opacity: 1 });
+      tl.to(el, { opacity: 0 });
+    });
+  },
+  setMission: () => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        pin: true,
+        scrub: true,
+      },
+    });
+  },
 };
 
 // FUNCTION gsap 애니메이션 등록
 const registAnimation = () => {
-  introAnimation();
-  setHeaderPosition();
+  animation.setIntro();
+  animation.setHeaderPosition();
 };
 
 (function () {
